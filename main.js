50daf5651540b50aab0dbe97739c8f32f5001160
FIRDAVS00222 <shoirajamilova5@gmail.com>
2024-09-06 00:18:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let sonlar = [2, 4, 9, 10, 45, 67, 8 , 90]
+let Juftsonlar =[2, 4, 10, , 8 ,90]
+let ToqSonlar =[9, 45, 67, ]
+ 
+
+
+ console.log('Juftson' +Juftsonlar);
+console.log('toqsonn' + ToqSonlar);
